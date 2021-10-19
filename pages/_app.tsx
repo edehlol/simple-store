ea4a68a6b3605823093c0bda44275017a4b8826d
edehlol <edymusajev@gmail.com>
2021-10-19 16:12:49 +0300

    added redux-persist functionality on cartReducer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
 import 'tailwindcss/tailwind.css';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import React, { useReducer, useState } from 'react';
-import { Cart } from '../types/Cart';
+import React from 'react';
+
 import { Provider } from 'react-redux';
-import { store } from '../redux/store';
+import { persistor, store } from '../redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
     </Provider>
   );
 }
--- redux/store.ts
+++ redux/store.ts
@@ -1,11 +1,36 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import cartReducer from './cartSlice';
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistCartConfig = {
+  key: 'root',
+  storage,
+};
+const persistedCartReducer = persistReducer(persistCartConfig, cartReducer);
 
 export const store = configureStore({
   reducer: {
-    cart: cartReducer,
+    cart: persistedCartReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
