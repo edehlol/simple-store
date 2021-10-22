9abf3f619d05963338b3822b50db17b3a96eabb6
edehlol <edymusajev@gmail.com>
2021-10-22 16:37:19 +0300

    added cloudinary cdn to mongodb for each product /  made a separate component for images

---
+++ src/components/Img.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+
+interface ImgProps {
+  src: string;
+  divClass?: string;
+  imgClass?: string;
+  alt?: string;
+}
+
+const Img = ({ src, divClass, imgClass, alt }: ImgProps) => {
+  return (
+    <div className={`relative ${divClass}`}>
+      <Image src={src} layout="fill" className={`object-contain ${imgClass}`} alt={alt} />
+    </div>
+  );
+};
+export default Img;
